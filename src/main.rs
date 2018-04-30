124304870b345c047581747c62ab328dbdf26a26
Dawson Israel <dawfre@amazon.com>
2018-04-30 13:26:09 -0300

    Add TODO command

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 extern crate clap;
 
-use clap::App;
+use clap::{App, SubCommand};
 
 fn main() {
-    App::new("Habitica")
+    let matches = App::new("Habitica")
         .version("0.1")
         .about("Unnoficial Habitica CLI")
         .author("Dawson Israel")
+        .subcommand(SubCommand::with_name("todo").about("List ToDo's taks"))
         .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("todo") {
+        println!("Foi");
+    }
 }
