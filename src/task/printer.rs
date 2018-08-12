4341c26a171589e27803396691fe1eaaf17f38f0
Dawson Israel <dawsonfi@gmail.com>
2018-08-12 10:52:44 -0300

    Add completed status and collors

--- src/task/printer.rs
+++ src/task/printer.rs
@@ -1,3 +1,6 @@
+extern crate colored;
+
+use self::colored::*;
 use habitica_rust_client::task::tasks::Task;
 
 pub trait Printer {
@@ -6,9 +9,16 @@ pub trait Printer {
 
 impl Printer for Task {
     fn print(&self) {
+
+        let completed = match self.is_completed() {
+            Some(_) => "true".green(),
+            None => "false".red(),
+        };
+
         println!(
-            "| {} | - {}",
-            self.get_task_type().as_ref().unwrap(),
+            "| {:5} | [{:5}] - {}",
+            self.get_task_type().as_ref().unwrap().cyan(),
+            completed,
             self.get_text().as_ref().unwrap()
         );
     }
